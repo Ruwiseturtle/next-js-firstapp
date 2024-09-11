74ddc919358f67447441821c02f4f1d0c75d3660
Matviienko Ruslana <brauniesrus@gmail.com>
2024-09-11 15:25:58 +0300

    Tailwind CSS

--- src/app/components/active-label.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export interface ActiveLabelProps{
-    children: React.ReactNode;
-}
-export default function ActiveLabel ({children}: ActiveLabelProps) {
-    return <span >{children}</span>
-}
